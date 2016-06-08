ff9f0e10ab69e5b9d11c2c928275d9fba2480c9b
suntorytime <stevensm90@gmail.com>
2016-06-08 16:52:36 -0700

    Got test API endpoint to show response

--- highlights/www/js/controllers.js
+++ highlights/www/js/controllers.js
@@ -2,7 +2,10 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats, $http) {
+  $http.get('http://jsonplaceholder.typicode.com/posts/1').then(function(response) {
+    $scope.myResponse = response.data.title;
+  })
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
