d3b36102caeab5b0b891d0431f5f74cb59bce295
Steven <stevensm90@gmail.com>
2016-06-09 16:09:44 -0700

    Integrate Yelp API

--- highlights/www/js/controllers.js
+++ highlights/www/js/controllers.js
@@ -2,40 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats, $http) {
-
-    var method =  'GET';
-    var url =     'http://api.yelp.com/v2/search';
-    var params = {
-      callback:                 'angular.callbacks._0',
-      location:                 '30030',
-      oauth_consumer_key:       '', // consumer key
-      oauth_token:              '', //Token
-      oauth_signature_method:   'HMAC-SHA1',
-      oauth_timestamp:          new Date().getTime(),
-      oauth_nonce:              randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-      term:                     'ramen'
-    }; // end params
-    var consumerSecret =        ''; //Consumer Secret
-    var tokenSecret =           ''; //Token Secret
-    var signature =
-      oauthSignature.generate(
-        method,
-        url,
-        params,
-        consumerSecret,
-        tokenSecret,
-        { encodeSignature: false }
-      );
-      // end signature
-    params['oauth_signature'] = signature;
-
-    $http(url, { params : params })
-      .then(function(response) {
-        $scope.myResponse = response.data
-      });
-    };
-
+.controller('ChatsCtrl', function($scope, Chats, MyYelpAPI) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -43,6 +10,12 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $scope.hello = "hello world!";
+  $scope.businesses = [];
+  MyYelpAPI.retrieveYelp('', function(data) {
+      $scope.businesses = data.businesses;
+
+  });
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
--- highlights/www/js/services.js
+++ highlights/www/js/services.js
@@ -1,5 +1,34 @@
 angular.module('starter.services', [])
 
+.factory("MyYelpAPI", function($http){
+  function randomString(length, chars) {
+      var result = '';
+      for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+      return result;
+  }; 
+  return {
+    "retrieveYelp": function(name, callback) {
+        var method = 'GET';
+        var url = 'http://api.yelp.com/v2/search';
+        var params = {
+            callback: 'angular.callbacks._0',
+            location: 'San+Francisco',
+            oauth_consumer_key: 'Ig9HVjb1RMMxSZiLRDRmLg', //Consumer Key
+            oauth_token: 'k1O1zkWGR__y6MdP6eg7jULU4fbaq5O9', //Token
+            oauth_signature_method: "HMAC-SHA1",
+            oauth_timestamp: new Date().getTime(),
+            oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+            term: 'ramen'
+            };
+        var consumerSecret = 'kQt5x38_RaK4T3EGJqh2nj7bZ3w'; //Consumer Secret
+        var tokenSecret = 'Y3Gtw8MLXr3GwTs1FJPiu6KXhbM'; //Token Secret
+        var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+        params['oauth_signature'] = signature;
+        $http.jsonp(url, {params: params}).success(callback);
+    }
+  }
+})
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
