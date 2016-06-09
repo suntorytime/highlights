0641ad4ffc5ed548764795beee98043012f7cef0
suntorytime <stevensm90@gmail.com>
2016-06-09 12:22:38 -0700

    Stage Yelp API

--- highlights/www/js/controllers.js
+++ highlights/www/js/controllers.js
@@ -3,9 +3,39 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats, $http) {
-  $http.get('http://jsonplaceholder.typicode.com/posts/1').then(function(response) {
-    $scope.myResponse = response.data.title;
-  })
+
+    var method =  'GET';
+    var url =     'http://api.yelp.com/v2/search';
+    var params = {
+      callback:                 'angular.callbacks._0',
+      location:                 '30030',
+      oauth_consumer_key:       '', // consumer key
+      oauth_token:              '', //Token
+      oauth_signature_method:   'HMAC-SHA1',
+      oauth_timestamp:          new Date().getTime(),
+      oauth_nonce:              randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+      term:                     'ramen'
+    }; // end params
+    var consumerSecret =        ''; //Consumer Secret
+    var tokenSecret =           ''; //Token Secret
+    var signature =
+      oauthSignature.generate(
+        method,
+        url,
+        params,
+        consumerSecret,
+        tokenSecret,
+        { encodeSignature: false }
+      );
+      // end signature
+    params['oauth_signature'] = signature;
+
+    $http(url, { params : params })
+      .then(function(response) {
+        $scope.myResponse = response.data
+      });
+    };
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
